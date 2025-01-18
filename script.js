e601356da4c9dcf74ad20cdce62b2d81c7503e47
athaarrvv <athaarrvv2@gmial.com>
2025-01-18 14:46:28 +0530

    fist move

---
+++ script.js
@@ -0,0 +1,37 @@
+const bar = document.getElementById("bar");
+const close = document.getElementById("close");
+const nav = document.getElementById("navbar");
+
+if (bar) {
+  bar.addEventListener("click", () => {
+    nav.classList.add("active");
+  });
+}
+if (close) {
+  close.addEventListener("click", () => {
+    nav.classList.remove("active");
+  });
+}
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  const urlParams = new URLSearchParams(window.location.search);
+  const productId = urlParams.get('id');
+
+  fetch('products.json')
+      .then(response => response.json())
+      .then(products => {
+          const product = products.find(p => p.id == productId);
+          if (product) {
+              document.getElementById('product-name').textContent = product.name;
+              document.getElementById('product-image').src = product.image;
+              document.getElementById('product-description').textContent = product.description;
+              document.getElementById('product-price').textContent = product.price;
+              document.getElementById('smallimg-1').src = smallimg[0];
+              document.getElementById('smallimg-2').src = product.smallimg[1];
+          } else {
+              document.getElementById('product-container').textContent = 'Product not found';
+          }
+      })
+      .catch(error => console.error('Error fetching product data:', error));
+});
